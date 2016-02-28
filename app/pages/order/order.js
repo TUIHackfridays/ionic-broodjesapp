524177f3a686082db91dd49b930053390b98bf50
dedulle wim <dedullewim@hotmail.com>
2016-02-28 14:09:57 +0100

    inject view controller

--- app/pages/order/order.js
+++ app/pages/order/order.js
@@ -1,13 +1,14 @@
-import {Page, NavParams} from 'ionic-framework/ionic';
+import {Page, NavParams, ViewController} from 'ionic-framework/ionic';
 
 @Page({
   templateUrl: 'build/pages/order/order.html'
 })
 export class OrderModal {
   static get parameters() {
-    return [[NavParams]];
+    return [[NavParams], [ViewController]];
   }
-  constructor(params) {
+  constructor(params, viewCtrl) {
+    this.viewCtrl = viewCtrl;
     this.title = params.get('title');
   }
   
